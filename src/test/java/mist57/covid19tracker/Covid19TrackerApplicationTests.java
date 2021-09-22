31851be8c797104d7d161595eda0cf09bff332d3
mist57 <vermadev.035@gmail.com>
2021-09-23 01:57:31 +0530

    Inital Commit

---
+++ src/test/java/mist57/covid19tracker/Covid19TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package mist57.covid19tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
